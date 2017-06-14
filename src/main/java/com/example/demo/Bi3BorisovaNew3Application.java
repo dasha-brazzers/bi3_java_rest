d04fd803061e8290cfe4b9d7540c0480eac6053e
Dasha Borisova <dddaaassshhhaaabbbooo@mail.ru>
2017-06-14 15:12:52 +0300

    booty hoes

---
+++ src/main/java/com/example/demo/Bi3BorisovaNew3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bi3BorisovaNew3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bi3BorisovaNew3Application.class, args);
+	}
+}
